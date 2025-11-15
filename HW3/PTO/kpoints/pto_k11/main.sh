6e9cfda2ebba4ab34c6f7f882d3af72a21cd36d7
chemerzhc <chemerzhc@gmail.com>
2025-11-15 04:10:00 -0500

    update HW3

---
+++ HW3/PTO/kpoints/pto_k11/main.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sbatch pto_k1.sh
+sleep 50
+sbatch pto_k2.sh
+sleep 50
+sbatch pto_k3.sh
+sleep 50
+sbatch pto_k4.sh
+sleep 50
+sbatch pto_k5.sh
+sleep 50
+sbatch pto_k6.sh
+sleep 50
+sbatch pto_k7.sh
+sleep 50
+sbatch pto_k8.sh
+sleep 50
+sbatch pto_k9.sh
+sleep 50
+sbatch pto_k10.sh
+sleep 50
+sbatch pto_k11.sh
+sleep 50
+sbatch pto_k12.sh
+sleep 50
